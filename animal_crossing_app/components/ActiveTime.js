a379de196587a8c752cfe2156ab4278470d580b9
Ryan Wong <ryane.wong@mail.utoronto.ca>
2020-12-23 22:40:06 -0500

    Added notes to load json data and Test graph in active time.js.

--- animal_crossing_app/components/ActiveTime.js
+++ animal_crossing_app/components/ActiveTime.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import {Text, View, StyleSheet} from "react-native";
+import {AppRegistry, StyleSheet, ScrollView , StatusBar, Text, View} from "react-native";
 import colors from '../Colors.js';
+import PieChart from 'react-native-pie-chart';
 
 class ActiveTime extends Component {
   constructor(props) {
@@ -14,15 +15,46 @@ class ActiveTime extends Component {
     
   }
 
+  // render() {
+  //   console.log(this.props);
+  //   var displayText;
+  //   if(this.props.displayText==="hello"){
+  //     displayText = "yes";
+  //   } else {
+  //     displayText = "no";
+  //   }
+  //   return <Text style={[styles.text,{backgroundColor: colors.textWhite[colors.mode]}]}>{this.props.displayText2}</Text>
+  // }
+
   render() {
-    console.log(this.props);
-    var displayText;
-    if(this.props.displayText==="hello"){
-      displayText = "yes";
-    } else {
-      displayText = "no";
-    }
-    return <Text style={[styles.text,{backgroundColor: colors.textWhite[colors.mode]}]}>{this.props.displayText2}</Text>
+    const chart_wh = 250
+    const series = [123, 321, 123, 789, 537]
+    const sliceColor = ['#F44336','#2196F3','#FFEB3B', '#4CAF50', '#FF9800']
+ 
+    return (
+      <ScrollView style={{flex: 1}}>
+        <View style={styles.container}>
+          <StatusBar
+            hidden={true}
+          />
+          <Text style={styles.title}>Basic</Text>
+          <PieChart
+            chart_wh={chart_wh}
+            series={series}
+            sliceColor={sliceColor}
+          />
+          <Text style={styles.title}>Doughnut</Text>
+          <PieChart
+            chart_wh={chart_wh}
+            series={series}
+            sliceColor={sliceColor}
+            doughnut={true}
+            coverRadius={0.45}
+            coverFill={'#FFF'}
+          />
+        </View>
+      </ScrollView>
+    );
   }
 }
 
@@ -38,6 +70,14 @@ const styles = StyleSheet.create({
     elevation: 5,
     marginTop: 100
   },
+  container: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  title: {
+    fontSize: 24,
+    margin: 10
+  }
 })
 
 export default ActiveTime;
