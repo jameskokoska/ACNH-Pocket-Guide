8f5e58fc7dbe8c9010ce74b3fea28d61589bb025
Ryan Wong <ryane.wong@mail.utoronto.ca>
2020-12-27 06:20:07 -0500

    isActive and parseActiveTime functions added.

--- animal_crossing_app/components/DateFunctions.js
+++ animal_crossing_app/components/DateFunctions.js
@@ -16,4 +16,36 @@ function getWeekDayShort(currentWeekDay){
   var weekDays = ['Sun','Mon','Tues','Wed','Thurs','Fri','Sat'];
   return weekDays[currentWeekDay];
 }
-export {getMonth, getWeekDay, getWeekDayShort};
+
+function parseActiveTime(activeTime){
+  // Format example: "4 AM"
+  const splitString = activeTime.split(" ");
+  const hour = splitString[0];
+  const ampm = splitString[1];
+  if(ampm==="PM"&&hour!=="12"){
+    hour = hour + 12
+  }
+  return hour
+}
+
+function isActive(activeTime){
+  // active time format: "4 AM – 9 PM"
+  const splitString = activeTime.split(" - ")
+  const currentHour = new Date().getHours();
+  const activeStart = parseInt(splitString[0]);
+  const activeEnd = parseInt(splitString[1]);
+  currentHour = parseInt(currentHour);
+  // check if it is available
+  if(activeTime!=="NA"||"All Day"){
+    // check if current time is between available hours
+    if(activeStart < currentHour < splitString){
+      return true
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
+}
+
+export {getMonth, getWeekDay, getWeekDayShort, isActive, parseActiveTime};
