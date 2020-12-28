426f4475055824bc681dcb69fc3599a4aad434a8
Ryan Wong <ryane.wong@mail.utoronto.ca>
2020-12-28 03:09:02 -0500

    temporary commit

--- animal_crossing_app/components/ActiveTime.js
+++ animal_crossing_app/components/ActiveTime.js
@@ -4,6 +4,25 @@ import colors from '../Colors.js';
 import Svg, { Circle, Rect } from 'react-native-svg';
 import {parseActiveTime, isActive} from "./DateFunctions"
 
+function buildMonthList(monthList){
+  return;
+}
+
+function getActiveTime(){
+  var hemispherePre = global.settingsCurrent[0]["currentValue"] === "true" ? "NH " : "SH "
+  var currentMonthShort = getMonthShort(new Date().getMonth());
+  console.log(item[hemispherePre+currentMonthShort]);
+  return;
+}
+
+function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
+  const angleInRadians = ((angleInDegrees - 90) * Math.PI) / 180.0;
+  return {
+    x: centerX + radius * Math.cos(angleInRadians),
+    y: centerY + radius * Math.sin(angleInRadians),
+  };
+}
+
 class ActiveTime extends Component {
   constructor(props) {
     super(props);
@@ -12,10 +31,15 @@ class ActiveTime extends Component {
   }
 
   render(){
-
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.get
+    var radius;
+    var centre;
+    var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
     console.log(this.props.item["Name"]);
+
     // test functions
-    const activeTime = this.props.item["NH Jan"];
+    const activeTime = this.props.item["NH Mar"];
     // console.log(activeTime);
     console.log(isActive(activeTime));
 
@@ -24,6 +48,11 @@ class ActiveTime extends Component {
       //   <Image source={require("../assets/icons/clockActive.png")} style={{height: 100, width: 100, resizeMode:'contain'}}/>
       // </View>
       <View>
+        {months.map((object, index) => {
+          return <Text key={index} style={{color: "white", position: "absolute"}}>
+            {object}
+            </Text>
+        })}
         <Image source={require("../assets/icons/months.png")} style={{height: 100, width: 100, resizeMode:'contain'}}/>
       </View>
     )
