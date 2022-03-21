a6dde470e60c8a0e60c1ec7b02ee678197eca801
James <50821962+jameskokoska@users.noreply.github.com>
2022-03-20 23:44:11 -0400

    added new supporter

--- animal_crossing_app/pages/CreditsPage.js
+++ animal_crossing_app/pages/CreditsPage.js
@@ -44,6 +44,7 @@ class CreditsPage extends Component {
       <CreditTextBox text="NoLNoodle"/>
       <CreditTextBox text="Harkken"/>
       <CreditTextBox text="RisingProtostar"/>
+      <CreditTextBox text="Kryptonite"/>
       <View style={{height: 5}}/>
       <View style={{paddingHorizontal: 20}}>
         <ButtonComponent vibrate={10} color={colors.dateButton[global.darkMode]} onPress={()=>{this.popupSupport?.setPopupVisible(true)}} text={capitalize(attemptToTranslate("Leave a Tip"))} />
